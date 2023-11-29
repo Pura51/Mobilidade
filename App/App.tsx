66598b2116e8fc12b6c70793905cadbc26050a3f
Pura51 <diogofernandes16547@gmail.com>
2023-11-29 23:12:51 +0000

    Formulação do cabeçalho
    Este commit, dedica-se para a inserção do cabeçalho inicial no projeto

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import cabecalho from "./css/cabecalho";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tp de Engenharia Software</Text>
-      <StatusBar style="auto" />
+      <View style={cabecalho.div1}></View>
+      <Text style={cabecalho.title}>IPCA Mobility Systems</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgreen',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#136935',
+    alignItems: 'center', // Alinhar no topo da página
+    justifyContent: 'flex-start', // Alinhar no topo da página
+    paddingTop: 50, // Ajuste conforme necessário para o espaçamento do topo
+    paddingLeft: 20, // Ajuste conforme necessário para o espaçamento lateral
   },
+
+
 });
+
+
+
---
+++ App/css/cabecalho.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const cabecalho = StyleSheet.create({  
+    div1: {
+      position: 'absolute',
+      width: '107%',
+      height: '18%',
+      backgroundColor: '#136935',
+      borderRadius: 25,
+    },
+  
+    title: {
+      backgroundColor: '#136935',
+      color: 'white',
+      paddingVertical: 25, // Ajuste conforme necessário para o espaçamento interno
+      paddingHorizontal: 20, // Ajuste conforme necessário para o espaçamento interno
+      fontSize: 30,
+    },
+  });
+
+  export default cabecalho;
