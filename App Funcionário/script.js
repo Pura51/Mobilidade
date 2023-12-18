54fb1cd02e6cf12f3f56d0eb3a797b010db0504b
anapintoo19 <anammpinto.fcp@gmail.com>
2023-12-18 23:52:05 +0000

    finaalização das páginas

--- App Funcionário/script.js
+++ App Funcionário/script.js
@@ -1,20 +1,4 @@
-// $(".menu > ul > li").click(function (e) {
-//     // remove active from already active
-//     $(this).siblings().removeClass("active");
-//     // add active to clicked
-//     $(this).toggleClass("active");
-//     // if has sub menu open it
-//     $(this).find("ul").slideToggle();
-//     // close other sub menu if any open
-//     $(this).siblings().find("ul").slideUp();
-//     // remove active class of sub menu items
-//     $(this).siblings().find("ul").find("li").removeClass("active");
-//   });
-  
-//   $(".menu-btn").click(function () {
-//     $(".sidebar").toggleClass("active");
-//   });
-  
+
   function toggleMenu(element) {
     // Adicione a lógica para mostrar/ocultar o menu ou execute outras ações
     console.log("Ícone do menu clicado para", element.nextElementSibling.innerText);
@@ -48,23 +32,7 @@ INPUT_PESQUISA.addEventListener('keyup', () =>{
   }
 });
 
-function loadContent(page) {
-  const contentDiv = document.querySelector('.home-content');
-  const xhttp = new XMLHttpRequest();
-  
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      contentDiv.innerHTML = this.responseText;
-    }
-  };
-  
-  if (page === 'home') {
-    window.location.href = 'index.html';
-  } else {
-    xhttp.open('GET', page + '.html', true);
-    xhttp.send();
-  }
-}
+
 
 function toggleDetail(detailId) {
   var detailElement = document.getElementById(detailId);
@@ -86,19 +54,5 @@ function toggleMenu(menu) {
   menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
 
-function removeClient(clientRow) {
-  console.log("Chamada para remover o cliente", clientRow);
-  var clientId = clientRow.getAttribute('data-id');
-  if (confirm('Tem certeza que deseja remover o cliente com ID ' + clientId + '?')) {
-      clientRow.remove();
-      // Aqui você pode adicionar a lógica para atualizar o back-end se necessário
-  }
-}
 
-function editClient(clientRow) {
-  var clientId = clientRow.getAttribute('data-id');
-  alert('Abrir formulário de edição para cliente com ID ' + clientId);
-  // Implemente a lógica para preencher um formulário de edição com os dados do cliente
-  // Isso pode incluir abrir um modal e preencher os campos do formulário
-}
 
